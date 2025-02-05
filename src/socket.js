83b273af30ecc003d6e2bc765bca7452d5a666b3
jeet sehgal <jeetsehgal786@gmail.com>
2025-02-05 18:28:15 +0530

    try to deploy

--- src/socket.js
+++ src/socket.js
@@ -1,11 +1,23 @@
-import {io} from "socket.io-client"
+// import {io} from "socket.io-client"
 
-export async function initSocket(){
-    const option={
-        'force new connection': true,
-        reconnectionAttempts : 1,
-        timeout: 10000,
-        transports: ['websocket'],
+// export async function initSocket(){
+//     const option={
+//         'force new connection': true,
+//         reconnectionAttempts : 1,
+//         timeout: 10000,
+//         transports: ['websocket'],
+//     };
+//     return io("wss://https://realtimecode-ypcz.onrender.com/",option)
+// }
+import { io } from "socket.io-client";
+
+export async function initSocket() {
+    const options = {
+        forceNew: true,
+        reconnectionAttempts: 3, // Try reconnecting 3 times
+        timeout: 10000, // 10 seconds timeout
+        transports: ["websocket"],
     };
-    return io("wss://https://realtimecode-ypcz.onrender.com/",option)
+
+    return io("wss://realtimecode-ypcz.onrender.com", options);
 }
